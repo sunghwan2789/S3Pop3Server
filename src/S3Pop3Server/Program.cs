dd7f6475b07f2e093407d392ec700c569556cf24
Sunghwan Bang <sunghwan2789@gmail.com>
2021-05-17 00:40:57 +0900

    S3 추가

--- src/S3Pop3Server/Program.cs
+++ src/S3Pop3Server/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -23,6 +26,15 @@ namespace S3Pop3Server
                     services.AddHostedService<Pop3Listener>();
                     services.AddTransient<Pop3ConnectionHandler>();
                     services.AddTransient<Pop3SessionHandler>();
+
+                    var awsOptions = hostContext.Configuration.GetAWSOptions();
+                    awsOptions.Credentials = new BasicAWSCredentials(
+                        hostContext.Configuration["AWS:AccessKey"],
+                        hostContext.Configuration["AWS:SecretKey"]);
+                    // https://stackoverflow.com/a/48312720
+                    // awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
+                    services.AddDefaultAWSOptions(awsOptions);
+                    services.AddAWSService<IAmazonS3>();
                 });
     }
 }
